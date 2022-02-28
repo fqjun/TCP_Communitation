08499d0fc651b61f2c554737c806cfe3578f8883
fqjun <1767851382@qq.com>
2022-02-28 17:34:49 +0800

    feat: add server of Linux version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # TCP_Communitation
 
 该分支为 Linux 版的内容
+
+        // "-lwsock32",
--- main_server.cpp
+++ main_server.cpp
@@ -1,84 +1,81 @@
 /**
  * @file 服务端
  * @author WCJJJ (1767851382@qq.com)
- * @brief 
- * @date 2022-02-26
- * 
+ * @brief
+ * @date 2022-02-28
+ *
  * commit:
  */
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include <WinSock2.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-#pragma comment(lib, "ws2_32.lib")
+#include <sys/socket.h>
+#include <sys/types.h>
 
 using namespace std;
 
-int main()
-{
-    WSADATA wsaData;
-    int port = 5099;
+int main(int argc, char *argv[]) {
+  int server_sockfd;  //服务器端套接字
+  int client_sockfd;  //客户端套接字
+  int len;
+  struct sockaddr_in my_addr;      //服务器网络地址结构体
+  struct sockaddr_in remote_addr;  //客户端网络地址结构体
+  int sin_size_int;
+  socklen_t sin_size;
+  char buf[BUFSIZ];                      //数据传送的缓冲区
+  memset(&my_addr, 0, sizeof(my_addr));  //数据初始化--清零
 
+  my_addr.sin_family = AF_INET;  //设置为IP通信
+  my_addr.sin_addr.s_addr =
+      INADDR_ANY;  //服务器IP地址--允许连接到所有本地地址上
+  my_addr.sin_port = htons(5699);  //服务器端口号
 
-    char buf[] = "Server: hello, I am a server.....，哈哈哈哈哈哈哈哈";
+  ///*创建服务器端套接字--IPv4协议，面向连接通信，TCP协议*/
+  if ((server_sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket error");
+    return 1;
+  } 
+  
+  /*将套接字绑定到服务器的网络地址上*/
+  if (bind(server_sockfd, (struct sockaddr *)&my_addr,
+           sizeof(struct sockaddr)) < 0) {
+    perror("bind error");
+    return 1;
+  } 
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        printf("Failed to load Winsock");
-        return 0;
+  /*监听连接请求--监听队列长度为5*/
+  if (listen(server_sockfd, 5) < 0) {
+    perror("listen error");
+    return 1;
+  };
+
+  sin_size_int = sizeof(struct sockaddr_in); 
+  sin_size = sin_size_int;
+
+  /*等待客户端连接请求到达*/
+  if ((client_sockfd = accept(server_sockfd, (struct sockaddr *)&remote_addr,
+                              &sin_size)) < 0) {
+    perror("accept error");
+    return 1;
+  }
+  printf("accept client %s/n", inet_ntoa(remote_addr.sin_addr));
+  len = send(client_sockfd, "Welcome to my server/n", 21,0);  //发送欢迎信息
+
+  /*接收客户端的数据并将其发送给客户端--recv返回接收到的字节数，send返回发送的字节数*/
+  while ((len = recv(client_sockfd, buf, BUFSIZ, 0)) > 0) {
+    buf[len] = '\0';
+    printf("%s/n", buf);
+    if (send(client_sockfd, buf, len, 0) < 0) {
+      perror("write error");
+      return 1;
     }
+  }
 
-    //创建用于监听的套接字
-    SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);
-
-    SOCKADDR_IN addrSrv;
-    addrSrv.sin_family = AF_INET;
-    addrSrv.sin_port = htons(port); //1024以上的端口号
-    addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-
-    int retVal = bind(sockSrv, (LPSOCKADDR)&addrSrv, sizeof(SOCKADDR_IN));
-    if (retVal == SOCKET_ERROR){
-        printf("Failed bind:%d\n", WSAGetLastError());
-        return 0;
-    }
-
-    if (listen(sockSrv, 10) == SOCKET_ERROR){
-        printf("Listen failed:%d", WSAGetLastError());
-        return 0;
-    }
-
-    SOCKADDR_IN addrClient;
-    int len = sizeof(SOCKADDR);
-
-//等待客户请求到来    
-        SOCKET sockConn = accept(sockSrv, (SOCKADDR *)&addrClient, &len);
-        if (sockConn == SOCKET_ERROR){
-            printf("Accept failed:%d", WSAGetLastError());
-            //break;
-        }
-
-        printf("Accept client IP:[%s]\n", inet_ntoa(addrClient.sin_addr));
-
-        //发送数据
-        int iSend = send(sockConn, buf, sizeof(buf), 0);
-        if (iSend == SOCKET_ERROR){
-            printf("send failed");
-           // break;
-        }
-
-        char recvBuf[100];
-        memset(recvBuf, 0, sizeof(recvBuf));
-        //         //接收数据
-        recv(sockConn, recvBuf, sizeof(recvBuf), 0);
-        printf("%s\n", recvBuf);
-
-        closesocket(sockConn);
-
-
-    closesocket(sockSrv);
-    WSACleanup();
-    system("pause");
-    return 0;
+  /*关闭套接字*/
+  close(client_sockfd);
+  close(server_sockfd);
+  return 0;
 }
